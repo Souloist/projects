89ee4a55513be13a5bf5d2c498072c56cfdc0c7e
Richard Shen <rich.shen@nyu.edu>
2016-02-06 01:13:11 -0500

    Update isBalanced.py

--- solutions/isBalanced.py
+++ solutions/isBalanced.py
@@ -1,14 +1,13 @@
-def isBalanced(string):
-	parenthesis = list(string)
+def isBalanced(parenthesis):
 
 	# Python arrays can be used as stacks due to append() and pop()
 	stack = []
 
-	for i in parenthesis:
+	for symbol in parenthesis:
 
 		# Push to stack every opening parenthesis
-		if i == "(":
-			stack.append(i)
+		if symbol in "({[":
+			stack.append(symbol)
 		else:
 
 			# If stack is empty, that means there is an extra closing parenthesis
@@ -16,13 +15,22 @@ def isBalanced(string):
 				return False
 
 			# Pop from stack if you see a closing parenthesis
-			stack.pop()
+			opening = stack.pop()
+
+			# Check to make sure it is the correct closing parenthesis
+			if not matches(opening,symbol):
+				return False
 
 	# If stack is not emtpy at this point, that means theres extra opening parenthesis
 
 	# If stack is empty, that means there were equal number of parenthesis
 	return True if len(stack) == 0 else False 
 
+def matches(opening, closing):
+	openers = "{(["
+	closers = "})]"
+
+	return openers.index(opening) == closers.index(closing)
 # test
-print isBalanced("(((((((())") # False
-print isBalanced("()(())(())") # True
+print isBalanced("((()]))") # False
+print isBalanced("{{([][])}()}") # True
