b99b52d9b023b437bc0b1aa28d840f9c3233009b
Richard Shen <rich.shen@nyu.edu>
2016-03-01 23:43:26 -0500

    added set_to_file and file_to_set methods

--- solutions/web_spyder/main.py
+++ solutions/web_spyder/main.py
@@ -44,3 +44,24 @@ def append_to_file(path, data):
 def delete_file_contents(path):
 	with open(path, 'w'):
 		pass
+
+# Convert file contents to set
+def file_to_set(file_name):
+	results = set()
+	with open(file_name, 'rt') as file:
+		for line in file:
+
+			# remember to remove the \n at the end of every line
+			results.add(line.replace('\n', ''))
+	return results
+
+# iterate thru set and place on new line in file
+def set_to_file(links, file):
+	
+
+	delete_file_contents(file)
+
+	# quality of life improvement
+	for link in sorted(links):
+		append_to_file(file,link)
+
