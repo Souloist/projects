90bfaaf0f5c8fa327fc0c3078741255ace47f8f2
Richard Shen <rich.shen@nyu.edu>
2016-03-20 23:37:45 -0400

    Create domain.py

---
+++ solutions/web_spyder/domain.py
@@ -0,0 +1,18 @@
+# this function will extract the domain name
+
+from urllib.parse import urlparse
+
+
+def get_domain_name(url):
+	try:
+		results = get_sub_domain_name(url).split('.')
+		return results[-2] + "." + results[-1]
+	except:
+		return '' 
+
+
+def get_sub_domain_name(url):
+	try:
+		return urlparse(url).netloc
+	except:
+		return ''
